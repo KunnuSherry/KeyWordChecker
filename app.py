b6f88fdc8d1788bd0dd06f2ad2455f42835437b3
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2024-09-08 16:54:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ from selenium.webdriver.chrome.options import Options
 app = Flask(__name__)
 
 def check_tag(link, tags):
-    # Set up Selenium
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     
     try:
